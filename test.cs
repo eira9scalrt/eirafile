d73dd1ed25501150096253d0fc75b2d5707ee62a
eira9scalrt <80973151+eira9scalrt@users.noreply.github.com>
2021-03-20 01:12:59 +0900

    Update test.cs

--- test.cs
+++ test.cs
@@ -2,6 +2,7 @@ using System;
 class ProgramTest{
 	public ProgramTest(){
 		Console.WriteLine("ドーナドーナのうた");	
+		Console.WriteLine("えっちじゃん");	
 	}
 	public static void Main(){
 		ProgramTest P1 = new ProgramTest();
