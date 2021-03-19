b6b3e6614b71a729b591e9eaa68a46c862f5f1c7
eira <iwasawamasami_ggo@yahoo.co.jp>
2021-03-20 00:48:37 +0900

    初めてのコミット

---
+++ test.cs
@@ -0,0 +1,11 @@
+using System;
+class ProgramTest{
+	public ProgramTest(){
+		Console.WriteLine("hello world");	
+	}
+	public static void Main(){
+		ProgramTest P1 = new ProgramTest();
+		
+	}
+	
+} 
